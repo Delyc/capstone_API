7e1e5638d06c8c0728407419bff0a07b0c246b05
Delyc <d.twizeyima@alustudent.com>
2023-12-10 21:57:01 +0200

    feat: adding security requirements

--- src/main/java/com/househunting/api/demo/DemoController.java
+++ src/main/java/com/househunting/api/demo/DemoController.java
@@ -2,7 +2,10 @@ package com.househunting.api.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +18,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/v1/demo")
-@SecurityRequirement(name = "bearerAuth")
-
+@Tag(name = "demo", description = "Demo API")
 public class DemoController {
+
+    @Operation(
+        summary = "Say hello", 
+        description = "Say hello to the world",
+        responses = {
+            @ApiResponse(
+                responseCode = "200", 
+                description = "Hello World!"
+            ),
+            @ApiResponse(
+                responseCode = "403", 
+                description = "Authentication failed"
+            )
+        }
+        )
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello World!");
