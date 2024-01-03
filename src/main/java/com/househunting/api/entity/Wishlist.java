7190464937ad1b6ba3fb8c13b91b8152ef308988
delyc <delyce35@gmail.com>
2024-01-03 02:38:24 +0200

    feat: defining wishlist entity

--- src/main/java/com/househunting/api/entity/Wishlist.java
+++ src/main/java/com/househunting/api/entity/Wishlist.java
@@ -2,12 +2,20 @@ package com.househunting.api.entity;
 
 import com.househunting.api.user.User;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wishlist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
