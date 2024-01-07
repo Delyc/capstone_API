2be49ef7c07f9a533a264b860cb0eceddf1d5b23
delyc <delyce35@gmail.com>
2024-01-07 22:20:20 +0200

    feat: creating agent availability

---
+++ src/main/java/com/househunting/api/entity/Availability.java
@@ -0,0 +1,35 @@
+package com.househunting.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import com.househunting.api.user.User;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Availability {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    // Other fields related to availability
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
+
