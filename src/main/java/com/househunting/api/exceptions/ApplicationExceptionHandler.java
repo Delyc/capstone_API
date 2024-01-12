84c5ad9e89603b9261ff334c12d29d629f95c935
delyc <delyce35@gmail.com>
2024-01-12 22:26:12 +0200

    feat: custom exceptions

---
+++ src/main/java/com/househunting/api/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.househunting.api.exceptions;
+
+package com.househunting.api.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DuplicateEmailException.class)
+    public Map<String, String> handleDuplicateEmailException(DuplicateEmailException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("email", ex.getMessage());
+        return errorMap;
+    }
+    
+}
