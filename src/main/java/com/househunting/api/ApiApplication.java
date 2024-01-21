591cf8b359d9fafcb7cf476b47c4cc4f3e5d5df9
delyc <delyce35@gmail.com>
2024-01-21 22:38:07 +0200

    feat: adding email exceptions

--- src/main/java/com/househunting/api/ApiApplication.java
+++ src/main/java/com/househunting/api/ApiApplication.java
@@ -2,16 +2,8 @@ package com.househunting.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableScheduling
-@ComponentScan
 public class ApiApplication {
 
 	public static void main(String[] args) {
